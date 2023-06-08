ab0d6926cf2735d750f6cb572280c8f5514c0525
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-06-08 12:41:28 +0100

    Find provincia added

--- src/main/java/com/casolimpo/api/services/ProviciaService.java
+++ src/main/java/com/casolimpo/api/services/ProviciaService.java
@@ -1,8 +1,22 @@
 package com.casolimpo.api.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.casolimpo.api.exceptions.ProvinciaException;
+import com.casolimpo.api.models.Provincia;
+import com.casolimpo.api.repositories.ProvinciaRepository;
 
 @Service
 public class ProviciaService {
 
+    @Autowired
+    private ProvinciaRepository provinciaRepository;
+
+    public Provincia findbyId(Integer idProvincia) {
+        return this.provinciaRepository
+                .findById(idProvincia)
+                .orElseThrow(() -> new ProvinciaException("Provincia não encontrada. "
+                        + "ID invalido id:" + idProvincia));
+    }
 }
